2edb69d289b4e16f5cb5064778d34c813e6b8a4f
leelk <leelk@leek-pc.wireless-1x.unc.edu>
2019-08-29 17:56:00 -0400

    Something happened.

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -12,28 +12,24 @@ public class A1Novice {
 		
 		int count = scan.nextInt();
 		
-		// [] first_last_amount = [count*3];
 		String[] first_names = new String[count];
+		
 		String[] last_names = new String[count];
+		
 		int[] items_bought = new int[count];
+		
 		double[] sum = new double[count];
-
 				
 		for(int i = 0; i<count; i++) {
 			first_names[i] = scan.next();
 			last_names[i] = scan.next();
 			items_bought[i] = scan.nextInt();
-			// System.out.println(first_names[i] + last_names[i] + items_bought[i]);
-			// int[] numbers_bought = new int[items_bought[i]];
-			// String[] name_item = new String[items_bought[i]];
-			// double[] price_item = new double[items_bought[i]];
-			System.out.println(items_bought[i]);
 				for (int n = 0; n<items_bought[i]; n++) {
 					double numbers_bought = scan.nextInt();
 					String name_item = scan.next();
 					double price_item = scan.nextDouble();
 					sum[i] = sum [i] + (numbers_bought * price_item);
-					// System.out.println(numbers_bought[n] + name_item[n] + price_item[n]);
+
 				}
 		}
 		
@@ -50,3 +46,8 @@ public class A1Novice {
 		// System.out.println((s1.toUpperCase()).charAt(0) + ". " + s1.substring(6) + (": "));
 	}
 }
+// System.out.println(first_names[i] + last_names[i] + items_bought[i]);
+// int[] numbers_bought = new int[items_bought[i]];
+// String[] name_item = new String[items_bought[i]];
+// double[] price_item = new double[items_bought[i]];
+// System.out.println(numbers_bought[n] + name_item[n] + price_item[n]);
