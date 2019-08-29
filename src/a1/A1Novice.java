960e124709ec19fde2c20a1007fdeacc28d8631b
leelk <leelk@leek-pc.wireless-1x.unc.edu>
2019-08-29 17:50:07 -0400

    Finished Novice, something changed in Example.

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -10,5 +10,43 @@ public class A1Novice {
 
 		// Your code follows here.
 		
+		int count = scan.nextInt();
+		
+		// [] first_last_amount = [count*3];
+		String[] first_names = new String[count];
+		String[] last_names = new String[count];
+		int[] items_bought = new int[count];
+		double[] sum = new double[count];
+
+				
+		for(int i = 0; i<count; i++) {
+			first_names[i] = scan.next();
+			last_names[i] = scan.next();
+			items_bought[i] = scan.nextInt();
+			// System.out.println(first_names[i] + last_names[i] + items_bought[i]);
+			// int[] numbers_bought = new int[items_bought[i]];
+			// String[] name_item = new String[items_bought[i]];
+			// double[] price_item = new double[items_bought[i]];
+			System.out.println(items_bought[i]);
+				for (int n = 0; n<items_bought[i]; n++) {
+					double numbers_bought = scan.nextInt();
+					String name_item = scan.next();
+					double price_item = scan.nextDouble();
+					sum[i] = sum [i] + (numbers_bought * price_item);
+					// System.out.println(numbers_bought[n] + name_item[n] + price_item[n]);
+				}
+		}
+		
+
+		scan.close();
+		
+		for (int i=0; i<count; i++) {
+			System.out.println(first_names[i].charAt(0) + ". " + last_names[i] + (": ") + sum[i]);
+		}
+		
+		// System.out.println(values);
+		// System.out.println(count);
+		// String s1 = "Smith Test";
+		// System.out.println((s1.toUpperCase()).charAt(0) + ". " + s1.substring(6) + (": "));
 	}
 }
